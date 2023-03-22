9baf6f0f1229a9393ffb36e4169ac3a2cb386dfa
Akhilesh <91828136+AkhilG87@users.noreply.github.com>
2023-03-23 03:13:03 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { verifyUser } = require('../helpers/verifyUser')
-const { Blog } = require('../models/blog')
+const { Blog } = require('../models/Blog')
 const { User } = require('../models/User')
 const router = express.Router()
 
